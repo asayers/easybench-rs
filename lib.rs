50aaec8b1f84473fcde5da52c59dfd53848c93c4
Alex Sayers <alex.sayers@gmail.com>
2017-03-31 12:38:18 +0900

    Make the mutable examples take longer

--- lib.rs
+++ lib.rs
@@ -20,17 +20,17 @@ println!("fib 200: {}", bench(|| fib(200) ));
 println!("fib 500: {}", bench(|| fib(500) ));
 
 // If a function needs to mutate some state, use `bench_env`.
-println!("reverse: {}", bench_env(vec![1,2,3], |xs| xs.reverse()));
-println!("sort:    {}", bench_env(vec![1,2,3], |xs| xs.sort()));
+println!("reverse: {}", bench_env(vec![0;100], |xs| xs.reverse() ));
+println!("sort:    {}", bench_env(vec![0;100], |xs| xs.sort()    ));
 ```
 
 Running the above yields the following results:
 
 ```none
-fib 200:         38 ns   (R²=1.000, 26053498 iterations in 155 samples)
-fib 500:        109 ns   (R²=1.000, 9131585 iterations in 144 samples)
-reverse:          3 ns   (R²=0.998, 23684997 iterations in 154 samples)
-sort:             3 ns   (R²=0.999, 23684997 iterations in 154 samples)
+fib 200:         38 ns   (R²=1.000, 26053497 iterations in 154 samples)
+fib 500:        110 ns   (R²=1.000, 9131584 iterations in 143 samples)
+reverse:         54 ns   (R²=0.999, 5669992 iterations in 138 samples)
+sort:            93 ns   (R²=1.000, 4685942 iterations in 136 samples)
 ```
 
 Easy! However, please read the caveats below before using.
@@ -270,8 +270,8 @@ mod tests {
     fn doctests_again() {
         println!("fib 200: {}", bench(|| fib(200) ));
         println!("fib 500: {}", bench(|| fib(500) ));
-        println!("reverse: {}", bench_env(vec![1,2,3], |xs| xs.reverse()));
-        println!("sort:    {}", bench_env(vec![1,2,3], |xs| xs.sort()));
+        println!("reverse: {}", bench_env(vec![0;100], |xs| xs.reverse()));
+        println!("sort:    {}", bench_env(vec![0;100], |xs| xs.sort()));
 
         // This is fine:
         println!("fib 1: {}", bench(|| fib(500) ));
